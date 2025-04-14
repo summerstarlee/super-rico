157c0a1f22e571afa081d2cb3c6811ba6981ec49
lishun <lishun@gongniu.cn>
2025-04-14 16:48:51 +0800

    update packageManager

---
+++ app/(chat)/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Chat",
+  description: "Chat with Super Rico",
+};
+
+export default function ChatPage() {
+  return <div>ChatPage</div>;
+}
